11f85b411a8bc0ffc3c1eeae376150f611e0b070
Dennis Ideler <dideler@findmypast.com>
2018-02-07 14:49:05 +0000

    Add basic help with -h flag

--- bin/mob.js
+++ bin/mob.js
@@ -1,3 +1,18 @@
 #! /usr/bin/env node
 
-console.log("it's alive!");
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv.h) {
+  const help = `
+Usage
+  $ git mob <co-author-initials>
+
+Options
+  -h  Prints this help and exits
+
+Examples
+  $ git mob jd  # Set John Doe is co-author
+  $ git mob jd am  # Set John and Amy as co-authors
+  `;
+  console.log(help);
+}
