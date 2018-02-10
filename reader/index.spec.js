59c7ca7ee005a607f7d10b88a5067debff2d484c
Richard Kotze <rkotze@findmypast.com>
2018-02-10 16:10:00 +0000

    Convert .git-author yaml file to json
    Decided to use js-yaml because has most npm download, github stars and
    fairly active repo updates.
    
    Compared with `yamljs` and `yaml-to-js`.

--- reader/index.spec.js
+++ reader/index.spec.js
@@ -1,18 +1,33 @@
-const test = require('ava');
-const { readSetup } = require('./index');
+const test = require("ava");
+const { readSetup } = require("./index");
 
-test('.gitauthor file does not exist', async t => {
+const validYaml = `
+authors:
+  jd: Jane Doe; jane
+  fb: Frances Bar
+email:
+  domain: findmypast.com`;
+
+test(".gitauthor file does not exist", async t => {
   const read = readSetup(() =>
-    Promise.reject(new Error('enoent: no such file or directory, open'))
+    Promise.reject(new Error("enoent: no such file or directory, open"))
   );
-  const error = await t.throws(read.gitAuthor());
+  const error = await t.throws(read.gitAuthors());
   t.regex(error.message, /enoent: no such file or directory, open/i);
 });
 
-test('read contents from .gitauthor', async t => {
-  const read = readSetup(() =>
-    Promise.resolve('authors: jd: john doe; jdoe@findmypast.com')
-  );
-  const author = await read.gitAuthor();
-  t.is(author, 'authors: jd: john doe; jdoe@findmypast.com');
+test("read contents from .gitauthor", async t => {
+  const read = readSetup(() => Promise.resolve(validYaml));
+
+  const author = await read.gitAuthors();
+  console.log(author);
+  t.deepEqual(author, {
+    authors: {
+      jd: "Jane Doe; jane",
+      fb: "Frances Bar"
+    },
+    email: {
+      domain: "findmypast.com"
+    }
+  });
 });
