44c8c83fb1ff663a262272ee4232715ff2e85e7c
Dennis Ideler <dideler@findmypast.com>
2018-03-08 18:21:41 +0000

    :sparkles: git-solo removes current mob from config
    Co-authored-by: Richard Kotze <rkotze@findmypast.com>

--- git-solo.spec.js
+++ git-solo.spec.js
@@ -21,26 +21,38 @@ test.after.always('cleanup', () => {
   deleteGitMessageFile();
 });
 
-test.serial('remove and unset co-authors', t => {
+test.serial('sets the current mob to the primary author', t => {
   addAuthor('Thomas Anderson', 'neo@example.com');
   setGitMessageFile();
+
   exec('git mob jd ea');
 
   const soloActual = exec('git solo').stdout.trimRight();
-  const soloExpected = stripIndent`
-    Thomas Anderson <neo@example.com>
-  `;
+  const soloExpected = 'Thomas Anderson <neo@example.com>';
 
-  const actual = eol.auto(
-    fs.readFileSync(process.env.GITMOB_MESSAGE_PATH, 'utf-8')
-  );
+  const mobActual = exec('git mob').stdout.trimRight();
+  const mobExpected = 'Thomas Anderson <neo@example.com>';
+
+  t.is(soloActual, soloExpected);
+  t.is(mobActual, mobExpected);
+
+  unsetCommitTemplate();
+});
+
+test.only.serial('removes co-authors from commit template', t => {
+  addAuthor('Thomas Anderson', 'neo@example.com');
+  setGitMessageFile();
+
+  exec('git mob jd ea');
+  exec('git solo');
+
+  const actual = eol.auto(fs.readFileSync(process.env.GITMOB_MESSAGE_PATH, 'utf-8'));
   const expected = eol.auto(stripIndent`
     A commit title
 
     A commit body that goes into more detail.`);
 
   t.is(actual, expected);
-  t.is(soloActual, soloExpected);
 
   unsetCommitTemplate();
 });
