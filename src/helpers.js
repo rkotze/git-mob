d8a2d2e5f5c4b12d3db0d3cec43e543d5f3c5754
Richard Kotze <richkotze@outlook.com>
2018-11-20 22:04:16 +0000

    Update helper to show examples of mob-print

--- src/helpers.js
+++ src/helpers.js
@@ -10,12 +10,13 @@ function runHelp() {
     Usage
       $ git mob <co-author-initials>
       $ git solo
+      $ git mob-print
 
     Options
       -h  Prints usage information
       -v  Prints current version
       -l  Prints list of available co-authors
-      
+
       --installTemplate  Installs a template file for prepare-commit-msg
 
     Examples
@@ -23,6 +24,7 @@ function runHelp() {
       $ git mob jd am  # Set John & Amy as co-authors
       $ git mob -l     # Show a list of all co-authors
       $ git solo       # Dissipate the mob
+      $ git mob-print  # Prints git-mob template to stdout. Used for prepare-commit-msg hook.
   `;
   console.log(message);
 }
