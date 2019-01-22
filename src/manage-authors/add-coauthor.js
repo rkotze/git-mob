6a69fbf44fd0b21c203a137ba6fd4e6bcfdd317b
Jack Bittiner <jackbittiner@hotmail.com>
2019-01-22 18:01:31 +0000

    :rotating_light: remove linting warnings

--- src/manage-authors/add-coauthor.js
+++ src/manage-authors/add-coauthor.js
@@ -3,7 +3,7 @@ const { gitAuthors } = require('../git-authors');
 async function addCoauthor(args) {
   const coauthors = gitAuthors();
   const authorList = await coauthors.read();
-  if (authorList.coauthors.hasOwnProperty(args[0])) {
+  if (Object.prototype.hasOwnProperty.call(authorList.coauthors, args[0])) {
     console.error(args[0] + ' already exists in .git-coauthors');
   } else {
     authorList.coauthors[args[0]] = { name: args[1], email: args[2] };
