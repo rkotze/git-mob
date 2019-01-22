24482496e32cf431a1c12e349a7b0fc4ab2ed136
Jack Bittiner <jackbittiner@hotmail.com>
2019-01-22 17:34:25 +0000

    :recycle: rename 'instance' for better readability
    Issue: #35

--- src/manage-authors/add-coauthor.js
+++ src/manage-authors/add-coauthor.js
@@ -1,13 +1,13 @@
 const { gitAuthors } = require('../git-authors');
 
 async function addCoauthor(args) {
-  const instance = gitAuthors();
-  const authorList = await instance.read();
+  const coauthors = gitAuthors();
+  const authorList = await coauthors.read();
   if (authorList.coauthors.hasOwnProperty(args[0])) {
     console.error(args[0] + ' already exists in .git-coauthors');
   } else {
     authorList.coauthors[args[0]] = { name: args[1], email: args[2] };
-    await instance.overwrite(authorList);
+    await coauthors.overwrite(authorList);
     console.log(args[1] + ' has been added to the .git-coauthors file');
   }
 }
