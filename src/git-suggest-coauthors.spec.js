1f6ea578af985dbb705115ac8be5c29cb8f9a9ea
Joel Chippindale <joel@joelchippindale.com>
2019-07-21 20:27:06 +0100

    Add a suggest-coauthors command
    Often the people you will be pairing with have already authored commits
    on the repo you are working on. This command makes it as easy as
    possible to add an existing author from the current repo to your list of
    coauthors.
    
    Example usage
    
    $ git suggest coauthors
    
    Here are some suggestions for coauthors based on existing authors of this repository
    
    git add-coauthor aa "Alice Apple" alice@acme.example
    git add-coauthor bo "Bob Orange" bob@acme.example
    git add-coauthor cb "Charlie Banana" charlie@acme.example
    
    Paste any line above into your console to add them as an author

---
+++ src/git-suggest-coauthors.spec.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+
+import {
+  exec,
+} from '../test-helpers';
+
+test('suggests potential coauthors', t => {
+  const { stdout } = exec('git suggest-coauthors');
+
+  t.regex(stdout, /Here are some suggestions/);
+  t.regex(stdout, /git add-coauthor rk "Richard Kotze" rkotze@findmypast.com/);
+  t.regex(stdout, /Paste any line above/);
+});
+
+test('-h prints help', t => {
+  const { stdout } = exec('git suggest-coauthors -h');
+
+  t.regex(stdout, /usage/i);
+  t.regex(stdout, /options/i);
+  t.regex(stdout, /example/i);
+});
+
