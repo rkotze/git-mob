9dc3d429c689cdaf19118b6add2ba088684cbc71
Richard Kotze <richkotze@outlook.com>
2022-08-21 18:53:43 +0100

    :zap: Move solo over to bundler
    Add minify option to config.

--- build/main.js
+++ build/main.js
@@ -1,23 +1,29 @@
 const esbuild = require('esbuild');
 const minimist = require('minimist');
 
+// Flags
+// -w: watch for file changes
+// -m: minify code - use for publish
 const argv = minimist(process.argv.slice(2),{
-  boolean: ['w'],
+  boolean: ['w', 'm'],
 
   alias: {
-    w: 'watch'
+    w: 'watch',
+    m: 'minify'
   },
 });
 
 const baseConfig = {
-  entryPoints: ['./src/git-mob.js'],
+  entryPoints: ['./src/git-mob.js', './src/solo.js'],
   mainFields: ['svelte', 'browser', 'module', 'main'],
   bundle: true,
   platform: "node",
   target: ["node14"],
   outdir: './dist',
+  minify: argv.minify,
   plugins: [],
-  logLevel: 'info'
+  logLevel: 'info',
+  external: ['common-tags', 'minimist', 'update-notifier']
 };
 
 if(argv.watch){
