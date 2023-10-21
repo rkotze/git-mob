92b10d92c6d3e26b76a156016205a085918d5e22
Richard Kotze <richkotze@outlook.com>
2023-10-21 16:36:44 +0100

    Convert check author to TS

--- packages/git-mob/src/check-author.spec.ts
+++ packages/git-mob/src/check-author.spec.ts
@@ -1,15 +1,18 @@
 import os from 'node:os';
 import test from 'ava';
-
+import type { Author } from 'git-mob-core';
 import { configWarning } from './check-author';
 
 test('does not print warning when config present', t => {
-  const actual = configWarning({ name: 'John Doe', email: 'jdoe@example.com' });
-  t.is(actual, undefined);
+  const actual = configWarning({
+    name: 'John Doe',
+    email: 'jdoe@example.com',
+  } as Author);
+  t.is(actual, '');
 });
 
 test('prints warning and missing config when one argument is missing', t => {
-  const actual = configWarning({ name: 'John Doe', email: '' });
+  const actual = configWarning({ name: 'John Doe', email: '' } as Author);
   const expected =
     'Warning: Missing information for the primary author. Set with:' +
     os.EOL +
@@ -18,7 +21,7 @@ test('prints warning and missing config when one argument is missing', t => {
 });
 
 test('prints warning and missing config when both arguments are missing', t => {
-  const actual = configWarning({ name: '', email: '' });
+  const actual = configWarning({ name: '', email: '' } as Author);
   const expected =
     'Warning: Missing information for the primary author. Set with:' +
     os.EOL +
